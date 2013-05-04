c7bf1cb95ab40d2943e845dde5de3f74ff9d296c
Thorben Schröder <stillepost@gmail.com>
2013-05-04 15:42:52 +0200

    Makes migration more forgiving

--- db/migrate/20130407111451_remove_unique_auth_indice.rb
+++ db/migrate/20130407111451_remove_unique_auth_indice.rb
@@ -2,7 +2,12 @@ class RemoveUniqueAuthIndice < ActiveRecord::Migration
   INDEX_NAME = 'index_owner_client_pairs'
 
   def up
-    remove_index :oauth2_authorizations, name: INDEX_NAME
+    begin
+      remove_index :oauth2_authorizations, name: INDEX_NAME
+    rescue StandardError => e
+      puts "Error occured during migration. Most likely just a missing index. Will carry on. Keep an eye on this!"
+      p e
+    end
     add_index :oauth2_authorizations, [:client_id, :oauth2_resource_owner_type, :oauth2_resource_owner_id], :name => INDEX_NAME
   end
 
